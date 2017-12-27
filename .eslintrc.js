1538e6c4ee16779249b2b961926a43f424437dec
黄涛(研发一部） <uid12403@sansi.com>
2017-12-27 13:43:36 +0800

    初始化，添加.editorconfig、.eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    "env": {
+        // "browser": true,
+        "node":true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        //缩进，4个空格
+        "indent": [
+            "error",
+            4
+        ],
+        //换行
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        //引号
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        //分号
+        "semi": [
+            "warn",
+            "always"
+        ]
+    }
+};
