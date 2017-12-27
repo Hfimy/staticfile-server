b04f7bb7643acaaadbaa50af0173df840002e538
黄涛(研发一部） <uid12403@sansi.com>
2017-12-27 15:32:58 +0800

    初步实现静态文件访问

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -1,5 +1,5 @@
 module.exports={
-    root:process.cwd(),
+    root:process.cwd(),//node启动程序所在目录
     hostname:'127.0.0.1',
     port:520
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 const http=require('http');
 const path=require('path');
+const fs=require('fs');
 const chalk=require('chalk');
 const {root,port,hostname}=require('./config/defaultConfig');
 
 //req,res都是流对象
 const server=http.createServer((req,res)=>{
     const filePath=path.join(root,req.url);
-
-    res.statusCode=200;
-    res.setHeader('Content-Type','text/html');
-    res.end(filePath);
+    fs.stat(filePath,(err,stats)=>{
+        if(err){
+            res.statusCode=404;
+            res.setHeader('Content-Type','text/plain');
+            res.end('This is not a directory or file');
+            return;
+        }
+        if(stats.isFile()){
+            res.statusCode=200;
+            res.setHeader('Content-Type','text/plain');
+            fs.createReadStream(filePath).pipe(res);
+        }else if(stats.isDirectory()){
+            fs.readdir(filePath,(err,files)=>{
+                res.statusCode=200;
+                res.setHeader('Content-Type','text/plain');
+                res.end(files.join('\n'));
+            });
+        }
+    });
 });
 
 server.listen(port,hostname,()=>{
