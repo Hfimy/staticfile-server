50ec93f1bd3a61ec1b61554b1476b03a7381045d
黄涛(研发一部） <uid12403@sansi.com>
2017-12-27 16:00:28 +0800

    简化异步操作，避免回调地狱

--- src/index.js
+++ src/index.js
@@ -1,35 +1,41 @@
-const http=require('http');
-const path=require('path');
-const fs=require('fs');
-const chalk=require('chalk');
-const {root,port,hostname}=require('./config/defaultConfig');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const chalk = require('chalk');
+const { root, port, hostname } = require('./config/defaultConfig');
+const promisify = require('util').promisify;
+
+const stat = promisify(fs.stat);
+const readdir = promisify(fs.readdir);
 
 //req,res都是流对象
-const server=http.createServer((req,res)=>{
-    const filePath=path.join(root,req.url);
-    fs.stat(filePath,(err,stats)=>{
-        if(err){
-            res.statusCode=404;
-            res.setHeader('Content-Type','text/plain');
-            res.end('This is not a directory or file');
-            return;
-        }
-        if(stats.isFile()){
-            res.statusCode=200;
-            res.setHeader('Content-Type','text/plain');
+const server = http.createServer(async (req, res) => {
+    const filePath = path.join(root, req.url);
+    try {
+        const stats = await stat(filePath);
+        if (stats.isFile()) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
             fs.createReadStream(filePath).pipe(res);
-        }else if(stats.isDirectory()){
-            fs.readdir(filePath,(err,files)=>{
-                res.statusCode=200;
-                res.setHeader('Content-Type','text/plain');
-                res.end(files.join('\n'));
-            });
+        } else if (stats.isDirectory()) {
+            const files = await readdir(filePath);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end(files.join('\n'));
         }
-    });
+    } catch (e) {
+        if (e) {
+            //eslint-disable-next-line no-console
+            console.error(chalk.red(e.message));
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/plain');
+            res.end('This is not a directory or file');
+        }
+    }
 });
 
-server.listen(port,hostname,()=>{
-    const addr=`http://${hostname}:${port}`;
+server.listen(port, hostname, () => {
+    const addr = `http://${hostname}:${port}`;
     //eslint-disable-next-line no-console
     console.info(`Server is running at ${chalk.green(addr)}`);
 });
